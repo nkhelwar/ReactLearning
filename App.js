2e9c29d4cb312158c248e4569e8cade48627c1c3
nkhelwar <nkhelwar@gmail.com>
2025-02-24 01:09:06 +0530

    pushing basic react code

---
+++ App.js
@@ -0,0 +1,23 @@
+
+const parent = React .createElement('div', {id: 'parent'},
+    React.createElement('div', {id: 'c1'},
+        [React.createElement('h1', null, 'H1 tag from c1'),
+        React.createElement('h2', null, 'H2 tag from c1')]
+    )
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
+
+/**
+ * <div id="parent">
+ *      <div id = "c1">
+ *          <h1>H1 tag from c1></h1>
+ *          <h2>H2 tag from c1></h2>
+ *      </div>
+ * </div>
+ * 
+ * 
+ * 
+ */
